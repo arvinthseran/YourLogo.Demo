45a16910e49a9b45fbf525a6305bf1ca62184930
Arvinth Seran <arvinth.seran@gmail.com>
2017-01-06 22:30:19 +0000

    included .gitignore file to ignore bin and obj directory

---
+++ YourLogo.Web.Driver/Drivers/ChromeYourLogoDriver.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace YourLogo.Web.Driver.Drivers
+{
+    public class ChromeYourLogoDriver : YourLogoDriver
+    {
+        private static int _pageNavigationTimeout = 10;
+
+        public ChromeYourLogoDriver()
+            : this(Environment.CurrentDirectory, _pageNavigationTimeout)
+        {
+
+        }
+        public ChromeYourLogoDriver(int pageNavigationTimeout)
+            : this(Environment.CurrentDirectory, pageNavigationTimeout)
+        {
+        }
+
+        public ChromeYourLogoDriver(string filepath)
+            : this(filepath, _pageNavigationTimeout)
+        {
+        }
+
+        public ChromeYourLogoDriver(string filepath, int pageNavigationTimeout)
+            : base(new ChromeDriver(filepath, Options()), pageNavigationTimeout)
+        {
+            _pageNavigationTimeout = pageNavigationTimeout;
+        }
+
+        private static ChromeOptions Options()
+        {
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("download.prompt_for_download", true);
+            options.AddArguments("no-sandbox");
+            return options;
+        }
+
+        Func<IWebElement, SelectElement> selectElement = (element) =>
+        {
+            return new SelectElement(element);
+        };
+
+
+        public override void Click(By @by)
+        {
+            FindElement(@by).Click();
+        }
+
+        public override void Click(IWebElement webelement)
+        {
+            webelement.Click();
+        }
+
+        public override void Click(Action action)
+        {
+            action();
+        }
+
+        public override void SendKeys(By @by, object text)
+        {
+            var webelement = FindElement(@by);
+            webelement.Clear();
+            webelement.SendKeys(text.ToString());
+        }
+
+        public override void SelectByValue(By @by, object text)
+        {
+            var webelement = selectElement(webdriver.FindElement(@by));
+            webelement.SelectByValue(text.ToString());
+        }
+
+        public override void SelectByText(By @by, object text)
+        {
+            var webelement = selectElement(webdriver.FindElement(@by));
+            if (!(webelement.Options.Any(x => x.Text == text.ToString())))
+            {
+                text = webelement.Options.FirstOrDefault(x => !(string.IsNullOrEmpty(x.Text))).Text;
+            }
+            webelement.SelectByText(text.ToString());
+        }
+
+        public override void SelectFromGroup(By by, object text)
+        {
+            var webelement = FindElements(@by);
+            IWebElement webelementtoSelectFromGroup;
+            if (webelement.Any(x => x.GetAttribute("title") == text.ToString()))
+            {
+                webelementtoSelectFromGroup = webelement.FirstOrDefault(x => x.GetAttribute("title") == text.ToString());
+            }
+            else
+            {
+                webelementtoSelectFromGroup = webelement.Where(x => !(string.IsNullOrEmpty(x.GetAttribute("title")))).FirstOrDefault();
+            }
+            Click(webelementtoSelectFromGroup);
+        }
+    }
+}
---
+++ YourLogo.Web.Test/Steps/Bindings/StepBindings.cs
@@ -0,0 +1,98 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using YourLogo.Web.Driver.Drivers;
+using YourLogo.Web.Driver.PageObject.Model;
+using TechTalk.SpecFlow.Assist;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YourLogo.Web.Test.Steps.Bindings
+{
+    [Binding]
+    public class StepBindings
+    {
+        private IYourLogoDriver _driver;
+
+        private readonly IObjectContainer _objectContainer;
+
+        private YourLogoPage yourlogoPage;
+
+        public StepBindings(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+            _driver = _objectContainer.Resolve<IYourLogoDriver>();
+            yourlogoPage = new YourLogoPage(_driver);
+        }
+
+        [Given(@"I am in the authentication page")]
+        public void GivenIAmInTheAuthenticationPage()
+        {
+            yourlogoPage.NaviagateToAuthPage();
+        }
+
+        [When(@"I login as a registered user")]
+        public void WhenILoginAsARegisteredUser()
+        {
+            yourlogoPage.Login("newuser@yourlogo.com", "passwd");
+        }
+
+        [Then(@"Login is successful")]
+        public void ThenLoginIsSuccessful()
+        {
+            Assert.True(yourlogoPage.CheckLogin(), "Login is sucessful");
+        }
+
+        [When(@"I register as a new user")]
+        public void WhenIRegisterAsANewUser(Table table)
+        {
+            dynamic newUserDetails = table.CreateDynamicInstance();
+            yourlogoPage.RegisterAsANewUser(newUserDetails);
+        }
+
+        [Then(@"Registration is successful")]
+        public void ThenRegistrationIsSuccessful()
+        {
+            Assert.True(yourlogoPage.CheckRegistration(),"Registration is not sucessful");
+        }
+
+        [When(@"I login as unauthorised user")]
+        public void WhenILoginAsUnauthorisedUser()
+        {
+            yourlogoPage.Login("User1@yourlogo.com", "passwd");
+        }
+
+        [Then(@"Login is unsuccessful")]
+        public void ThenLoginIsUnsuccessful()
+        {
+            Assert.False(yourlogoPage.CheckLogin(), "Login is sucessful");
+        }
+
+        [When(@"I add Items to the basket")]
+        public void WhenIAddItemsToTheBasket(Table table)
+        {
+            List<dynamic> cartItems = table.CreateDynamicSet().ToList();
+            cartItems.ForEach(x => yourlogoPage.AddItemsToCart(x));
+        }
+
+        [Then(@"I can review the items from the basket")]
+        public void ThenICanReviewTheItemsFromTheBasket()
+        {
+            yourlogoPage.ClickCheckOut();
+            Assert.True(yourlogoPage.CheckCartSummary(), "We have not arrived at Shopping - cart summary page");
+        }
+
+        [When(@"I purchase Item from the basket")]
+        public void WhenIPurchaseItemFromTheBasket()
+        {
+            yourlogoPage.ClickCheckOut();
+            yourlogoPage.PurchaseItemsInTheCart();
+        }
+
+        [Then(@"Checkout is successful")]
+        public void ThenCheckoutIsSucessful()
+        {
+            Assert.True(yourlogoPage.CheckOrderConfirmation(), "We are not arrved at Order Confirmation page");
+        }
+    }
+}
